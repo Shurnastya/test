7bbe877a94ceebf1bc1ffe9792505f6b6515b8f8
Anastasiya <nastja_1998-12@mail.ru>
2022-07-01 02:14:12 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,22 @@
+function popup () {
+    const popupOpen = document.getElementById('popup-open');
+    const popupClose = document.getElementById('popup-close');
+    const popup = document.getElementById('popup');
+
+    popupOpen.addEventListener('click', function(e) {
+        e.preventDefault();
+        popup.classList.add('active');
+    });
+
+    popupClose.addEventListener('click', () => {
+        popup.classList.remove('active');
+    });
+}
+popup();
+
+// Маска номер
+var element = document.getElementById('phone');
+var maskOptions = {
+  mask: '+{7}(000)000-00-00'
+};
+var mask = IMask(element, maskOptions);
